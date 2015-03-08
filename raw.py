ea5dc7654c1b45084ee53a8ea392b87ed3431487
preich <preich@192.168.1.101>
2015-03-08 14:18:53 +0000

    cleaned

--- raw.py
+++ raw.py
@@ -1,43 +1,8 @@
 import sys
 import time
 import mraa
-#import numpy
 
-from math import sqrt
 
-def mean(lst):
-    """calculates mean"""
-    sum = 0
-    for i in range(len(lst)):
-        sum += lst[i]
-    return (sum / len(lst))
-
-def stddev(lst):
-    """calculates standard deviation"""
-    sum = 0
-    mn = mean(lst)
-    for i in range(len(lst)):
-        v = lst[i] - mn
-        sum += v*v #(lst[i]-mn),2)
-    sz = len(lst) - 1 
-#    print "sum: " + str(sum) + ", len: " + str(sz)
-    
-    s = sqrt(sum/sz)
-    return s
-
-numbers = [120,112,131,211,312,90]
-
-def report(ar):
-  if len(ar) > 1:
-    return str(len(ar)) + "," + str(mean(ar)) + "," + str(stddev(ar))
-  else:
-    return "NA,NA,NA"
-#print stddev(numbers)
-
-arx = []
-ary = []
-old_time = 0
-#print (mraa.getVersion())
 f = open('raw.csv','w')
 f.write("time,a0,a1,a4,a5\n")
 stime = time.time()
