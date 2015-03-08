3c0d7a9794b1086c4ae1ac16e7b4b6bb21825640
preich <preich@192.168.1.101>
2015-03-08 14:18:07 +0000

    latest

--- raw.py
+++ raw.py
@@ -39,17 +39,16 @@ ary = []
 old_time = 0
 #print (mraa.getVersion())
 f = open('raw.csv','w')
-f.write("time,a0,a5")
+f.write("time,a0,a1,a4,a5\n")
 stime = time.time()
 duration = float(sys.argv[1])
 
 i = 0
 try:
-    x = mraa.Aio(0)
-    y = mraa.Aio(5)
+    ar = [mraa.Aio(x) for x in [0,1,4,5]]
     flag = True
     while flag:
-      line = str(time.time()) + "," + str(x.read()) + "," + str(x.read())
+      line = str(time.time()) + "," + ",".join([str(x.read()) for x in ar])
       f.write(line + "\n")
       i = i + 1
       since = time.time() - stime
